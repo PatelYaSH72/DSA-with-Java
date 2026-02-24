74a6bc66f7eea7ce9ac3f7e9f8eec23baf394d1e
Yashkumar Vishnubhai Patel <yp7201007221@gmail.com>
2026-02-24 13:34:19 +0530

    Add files via upload

---
+++ java/operators1.java
@@ -0,0 +1,21 @@
+/*
+What will be the output of the following programs :
+*/
+
+
+package javabasics;
+
+public class operators1 {
+  public static void main(String[] args) {
+    int x = 2, y = 5;
+    int exp1 = (x * y / x);
+    int exp2 = (x * (y / x));
+    System.out.print(exp1 + ",");
+    System.out.print(exp2);
+  }
+
+}
+
+/*
+output: 5,4
+*/
