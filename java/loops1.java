c6a3ed8cf7d43f44280191de850f3256a40565d4
Yashkumar Vishnubhai Patel <yp7201007221@gmail.com>
2026-02-26 20:28:07 +0530

    Add files via upload

---
+++ java/loops1.java
@@ -0,0 +1,17 @@
+/*
+ How many times 'Hello' is printed?
+*/
+
+public class loops1 {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Hello");
+      i += 2;
+    }
+  }
+}
+
+/*
+output: HELLO
+        HELLO
+*/
