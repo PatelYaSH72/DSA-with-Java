7f22a55bbe7783a1674412821c99c5657e93826d
Yashkumar Vishnubhai Patel <yp7201007221@gmail.com>
2026-03-02 19:47:48 +0530

    Add files via upload

---
+++ java/pattern10.java
@@ -0,0 +1,35 @@
+/*
+NUMBER PYRAMID pattern
+*/
+
+public class pattern10 {
+  
+    public static void main(String[] args) {
+      int a = 5;
+     
+
+      for(int i = 1; i <= a; i++){
+
+        for(int j = a-1; j >= i; j--){
+          System.out.print(" ");
+        }
+
+          for(int j = 1; j <= i; j++){
+              System.out.print(i);
+              System.out.print(" ");
+          }
+           
+          System.out.println();
+
+      }
+    }
+
+}
+
+/*
+output:     1 
+           2 2
+          3 3 3
+         4 4 4 4
+        5 5 5 5 5
+*/
